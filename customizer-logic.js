053a6e4cd46ed4fa3af732bb23f67bda8754df67
Yeboka <89529214+Yeboka@users.noreply.github.com>
2022-12-13 13:41:46 +0600

    Customizer logic

---
+++ customizer-logic.js
@@ -0,0 +1,22 @@
+
+function addToCard () {
+    const buttons = document.getElementsByTagName("input");
+    let product_name = document.getElementById("product-name").innerHTML;
+    let price = document.getElementsByTagName("cost")[0].innerHTML;
+    let size;
+    let color;
+
+    for (let i = 0; i < buttons.length; i++) {
+        if (buttons[i].type === "radio" && buttons[i].name === "size") {
+            if (buttons[i].checked) {
+                size = buttons[i].value;
+            }
+        } else if (buttons[i].type === "radio" && buttons[i].name === "color") {
+            if (buttons[i].checked) {
+                color = buttons[i].value;
+            }
+        }
+    }
+
+    console.log("Product-name: " + product_name + " \nPrice: " + price + " \nSize: " + size + " \nColor: " + color);
+}
