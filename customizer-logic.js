60fb7fa30076f04562db5eb06275f11445e49b1b
Yeboka <89529214+Yeboka@users.noreply.github.com>
2022-12-19 16:09:21 +0600

    tying php and js

--- customizer-logic.js
+++ customizer-logic.js
@@ -1,9 +1,12 @@
 
 const hoodies = ["assets/closes/hoodie/hoodie-black-minilogo.jpg", "assets/closes/hoodie/hoodie-blue-minilogo.jpg", "assets/closes/hoodie/hoodie-white-minilogo.jpg"]
+let idx = 0;
+
 function addToCard () {
     const buttons = document.getElementsByTagName("input");
     let product_name = document.getElementById("product-name").innerHTML;
     let price = document.getElementsByTagName("cost")[0].innerHTML;
+    let href = document.getElementById("p-img").src;
     let size;
     let color;
 
@@ -19,7 +22,19 @@ function addToCard () {
         }
     }
 
-    console.log("Product-name: " + product_name + " \nPrice: " + price + " \nSize: " + size + " \nColor: " + color);
+    console.log("Product-name: " + product_name + " \nPrice: " + price + " \nSize: " + size + " \nColor: " + color + " \nhref: " + href);
+
+    fetch('/api/insert.php', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: `type=${product_name}&cost=${prize}&size=${size}&color=${color}&href=${href}`
+    })
+        .then(response => response.json())
+        .then(data => console.log(data));
+
+    idx++;
 }
 
 function changeColor (e) {
